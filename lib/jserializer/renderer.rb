e4b10216d0842f82eb2be4cfba2031b206370aa5
Andrius Chamentauskas <andrius.chamentauskas@gmail.com>
2015-03-11 15:08:31 +0200

    Rails 4.2 compatibility

--- lib/jserializer/renderer.rb
+++ lib/jserializer/renderer.rb
@@ -1,11 +1,13 @@
 module JSerializer::Renderer
-  def _render_option_json(resource, options)
-    serializer = JSerializer.for(resource, **options.merge(controller: self))
+  class_eval <<-RUBY
+    def #{ActionController::Renderers._render_with_renderer_method_name(:json)}(resource, options)
+      serializer = JSerializer.for(resource, **options.merge(controller: self))
 
-    if serializer
-      super(serializer, options)
-    else
-      super
+      if serializer
+        super(serializer, options)
+      else
+        super
+      end
     end
-  end
+  RUBY
 end
